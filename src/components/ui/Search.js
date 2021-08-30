8837cec7e93cf345d93419b9ab50bd94caf8d383
Kadam <shubham.kadam@int.asurion.com>
2021-08-30 11:00:34 +0530

    BB

---
+++ src/components/ui/Search.js
@@ -0,0 +1,27 @@
+import { getSuggestedQuery } from '@testing-library/react'
+import React,{useState} from 'react'
+
+const Search = ({getQuery}) => {
+
+    const[text,setText] = useState('')
+
+    const onChange = (q) => {
+        setText(q)
+        getSuggestedQuery(q)
+    }
+    return (
+        <section className='search'>
+            <form>
+                <input type='text'
+                className='form-control'
+                placeholder='Search characters'
+                value={text}
+                onChange={(e)=>onChange(e.target.value)}
+                autofocus
+                />
+            </form>
+        </section>
+    )
+}
+
+export default Search
